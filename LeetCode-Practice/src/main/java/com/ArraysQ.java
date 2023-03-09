2b5cb58cd3ef057d4ac97cac0c28310f1d5655d6
jyotikashyap <jyotikas1608@gmail.com>
2023-03-09 19:14:22 +0530

    arrayQ

--- LeetCode-Practice/src/main/java/com/ArraysQ.java
+++ LeetCode-Practice/src/main/java/com/ArraysQ.java
@@ -67,7 +67,25 @@ public class ArraysQ {
        // System.out.println(minJumps(arr));
         int [] books={12 ,34, 67, 90};
        // System.out.println(findPages(books,books.length,5));
+        int num = Integer.MAX_VALUE;
+        reverse(num);
+    }
 
+    //
+    public static int reverse(int n) {
+        int rem=0; int sum=0;
+        while(n!=0) {
+            rem=n%10;
+            n=n/10;
+            if(sum>Integer.MAX_VALUE/10 || (sum==Integer.MAX_VALUE/10 && rem>7)) {
+                return 0;
+            }
+            if(sum<Integer.MIN_VALUE/10 || (sum==Integer.MIN_VALUE/10 && rem<-8)) {
+                return 0;
+            }
+            sum=sum*10+rem;
+        }
+        return sum;
     }
 
     //Allocate minimum number of pages
